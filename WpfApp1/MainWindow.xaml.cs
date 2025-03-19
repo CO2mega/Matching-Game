13d490a78e82094579c974ccc88348dbb2dd49f0
CO2mega <1652737510@qq.com>
2025-03-19 19:50:34 +0800

    添加项目文件。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
